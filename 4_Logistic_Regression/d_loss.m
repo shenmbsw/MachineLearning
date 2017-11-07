6c4ed42344a102b0ca1d6931a43b31f5c9aedde8
shenmbsw <shenmbsw@gmail.com>
2017-11-07 10:24:32 -0500

    Nov 7 SVM

--- 4_Logistic_Regression/d_loss.m
+++
@@ -1,8 +0,0 @@
-function [ dloss ] = d_loss( x,y,w, lambda )
-    m = size(w,2);
-    n = size(x,2);   
-    p = softmax(w.'*x) - ind2vec(y);
-    dloss = x*p.';
-    dloss = dloss + lambda * w;    
-end
-
